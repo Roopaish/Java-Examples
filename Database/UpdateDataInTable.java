43c1aeba2cd4a126a168daf51e1a73291fa4370e
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-04-03 13:43:13 +0545

    exceptions

--- Database/UpdateDataInTable.java
+++ Database/UpdateDataInTable.java
@@ -12,10 +12,10 @@
 import java.sql.*;
 
 public class UpdateDataInTable {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException, ClassNotFoundException {
     String url = "jdbc:mysql://localhost:/3306/students";
 
-    Class.forName("com.mysql.jdbc.Driver");                                       // Register driver
+    Class.forName("com.mysql.jdbc.Driver");                                       // Register driver, ClassNotFoundException
     Connection conn = DriverManager.getConnection(url, "root", "root");           // Connect to database
     Statement statement = conn.createStatement();                                 // Create statement
     String query = "UPDATE students SET name = 'John', age = 20 WHERE id = 1";    // Prepare Query
