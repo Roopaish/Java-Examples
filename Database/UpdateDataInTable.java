23a682e58a889bcad9f7683f09b63298a644856b
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-04-03 14:46:44 +0545

    display data in tabel

--- Database/UpdateDataInTable.java
+++ Database/UpdateDataInTable.java
@@ -13,7 +13,7 @@ import java.sql.*;
 
 public class UpdateDataInTable {
   public static void main(String[] args) throws SQLException, ClassNotFoundException {
-    String url = "jdbc:mysql://localhost:/3306/students";
+    String url = "jdbc:mysql://localhost:3306/students";
 
     Class.forName("com.mysql.jdbc.Driver");                                       // Register driver, ClassNotFoundException
     Connection conn = DriverManager.getConnection(url, "root", "root");           // Connect to database
