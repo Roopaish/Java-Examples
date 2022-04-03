cc14ab75539e267846726fea6134cf5fd298e072
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-04-03 16:40:56 +0545

    audio player

---
+++ Applet/AudioPlayer.java
@@ -0,0 +1,41 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.Action;
+
+public class AudioPlayer extends Applet implements ActionListener {
+  AudioClip audio;
+  Button play, stop, loop;
+
+  @Override
+  public void init() {
+    System.out.println("Initializing...");
+    audio = getAudioClip(getDocumentBase(), "audio.au");
+    setSize(500, 500);
+    setLayout(new FlowLayout());
+    setVisible(true);
+
+    play = new Button("Play");
+    stop = new Button("Stop");
+    loop = new Button("Loop");
+    add(play);
+    add(stop);
+    add(loop);
+
+    play.addActionListener(this);
+    stop.addActionListener(this);
+    loop.addActionListener(this);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == play) {
+      audio.play();
+    } else if (e.getSource() == stop) {
+      audio.stop();
+    } else if (e.getSource() == loop) {
+      audio.loop();
+    }
+  }
+}
